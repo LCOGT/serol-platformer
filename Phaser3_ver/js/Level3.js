8eaa50b87a596d92400bf2db9dc87d652f2bf0c4
Veronika Kravchenko <kravchenkov@cardiff.ac.uk>
2019-08-23 10:54:17 +0100

    another attempt at stars

--- Phaser3_ver/js/Level3.js
+++ Phaser3_ver/js/Level3.js
@@ -78,16 +78,16 @@ class Level3 extends Phaser.Scene {
 
     //  Mash 4 images together to create background
     // this.add.image(0, 0, 'dark_sky').setOrigin(0);
-    this.add.image(0, 0, 'stars_bg').setOrigin(0);
+    this.add.image(0, 0, 'stars_bg');
 
     // this.add.image(config.scale.width, 0, 'dark_sky').setOrigin(0).setFlipX(true);
-    this.add.image(1024, 0, 'stars_bg').setOrigin(0).setFlipX(true);
+    this.add.image(1024, 0, 'stars_bg');
 
     // this.add.image(0, config.scale.height, 'dark_sky').setOrigin(0).setFlipY(true);
-    this.add.image(0, 640, 'stars_bg').setOrigin(0).setFlipY(true);
+    this.add.image(0, 640, 'stars_bg');
 
     // this.add.image(config.scale.width, config.scale.height, 'dark_sky').setOrigin(0).setFlipX(true).setFlipY(true);
-    this.add.image(1024, 640, 'stars_bg').setOrigin(0).setFlipX(true).setFlipY(true);
+    this.add.image(1024, 640, 'stars_bg');
     
     //astronomical objects
     this.astros = this.physics.add.group();
